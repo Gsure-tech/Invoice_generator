9bcae436bf6023c4ccbaa7a8120e99f5a5fdbe1e
gsure-tech <Geeescode@>
2025-01-14 00:03:00 +0100

    add line between the name and date to pdf

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ for filepath in filepaths:
     pdf.cell(w=50, h=8, txt=f"Invoice nr.{invoice_nr}", ln=1)
 
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"Invoice nr.{date}")
+    pdf.cell(w=50, h=8, txt=f"Date: {date}")
 
 
     pdf.output(f"PDFs/{filename}.pdf")
