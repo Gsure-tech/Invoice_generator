ac1b402fa2ad944d4324fba4f76290d6862a7058
gsure-tech <Geeescode@>
2025-01-13 23:11:51 +0100

    implement student task

--- task_25.py
+++ task_25.py
@@ -9,9 +9,12 @@ pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
     pdf.add_page()
+
     filename = Path(filepath).stem
+
     pdf.set_font(family="Times", size=16, style="B")
     pdf.cell(w=50, h=8, txt=f"{filename}")
-    pdf.output(f"PDFs/{filename}.pdf")
+
+pdf.output(f"PDFs/{filename}.pdf")
 
 
